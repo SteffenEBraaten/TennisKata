fd2e22b68afb6415bbb342c75401f2140edb98eb
SteffenEBraaten <steffen.e.braaten@gmail.com>
2020-05-11 20:37:54 +0200

    Made a working version of the kata

---
+++ src/main/kotlin/tennis/GameStatus.kt
@@ -0,0 +1,8 @@
+package tennis
+
+enum class GameStatus {
+    SCORE,
+    DEUCE,
+    ADVANTAGE,
+    WIN
+}
---
+++ src/main/kotlin/tennis/Player.kt
@@ -0,0 +1,3 @@
+package tennis
+
+data class Player(val playerName: String, var score: Int=0)
